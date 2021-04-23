eb1fb9865427a609dd75451295c3ce7bfd4c2323
Prabhat Roshan <prabhat9546784170@gmail.com>
2021-04-23 20:08:55 +0530

    Add files via upload

---
+++ Daily_practice_codes/if-elif-else/user_input.py
@@ -0,0 +1,20 @@
+# Write a python program to check the user input abbreviation.If the user enters "lol", 
+# print "laughing out loud".If the user enters "rofl", print "rolling on the floor laughing".
+# If the user enters "lmk", print "let me know".If the user enters "smh", print "shaking my head".
+
+user_ip=input("Enter the user input--")
+
+if user_ip=="lol":
+    print("laughing out loud")
+
+elif user_ip=="rofl":
+    print("rolling on the floor laughing")
+
+elif user_ip=="lmk":
+    print("let me know")
+
+elif user_ip=="smh":
+    print("shaking my head")
+
+else:
+    print("enter another i/p")
