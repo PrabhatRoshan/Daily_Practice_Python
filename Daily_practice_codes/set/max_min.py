5b4d7aeef48c5d7bffdd40989de10c5192728e75
Prabhat Roshan <prabhat9546784170@gmail.com>
2021-04-26 15:07:02 +0530

    Add files via upload

---
+++ Daily_practice_codes/set/max_min.py
@@ -0,0 +1,7 @@
+# Write a Python program to find maximum and the minimum value in a set
+
+my_set = set([7,3,15,2,8,31,21,6])
+
+print("Maximum value of the set is: ",max(my_set))
+
+print("minimum value of the set is: ",min(my_set))
